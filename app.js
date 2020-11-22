0573f827dbaaf4a1e3edd52a73daac289676f0c4
diebjackal <55783877+diebjackal@users.noreply.github.com>
2020-11-22 14:36:16 +0900

    [ADD] contact page

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.get("/about", function(req, res) {
     res.render("about", { startingContent: aboutContent });
 })
 
-
-
+app.get("/contact", function(req, res) {
+    res.render("contact", { startingContent: contactContent });
+})
 
 
 
